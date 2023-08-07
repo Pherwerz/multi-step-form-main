1e4e8971911fcc69dc2fa87e1b2a104e44d70c1a
Fawaz Adebayo <fawaz.adebayo.ajibola@gmail.com>
2023-08-07 08:28:09 +0100

    info almost done

--- components/info/index.tsx
+++ components/info/index.tsx
@@ -1,15 +1,6 @@
 import { FC } from 'react';
 import './index.scss';
 
-// Name
-// e.g. Stephen King
-
-// Email Address
-// e.g. stephenking@lorem.com
-
-// Phone Number
-// e.g. +1 234 567 890
-
 const Info: FC = () => {
   return (
     <div className="info">
@@ -18,6 +9,50 @@ const Info: FC = () => {
       <p className="p">
         Please provide your name, email address, and phone number.
       </p>
+
+      <div className="info-box">
+        <div className="mb-[20px]">
+          <div className="info-label">
+            <label htmlFor="name">Name</label>
+            <p></p>
+          </div>
+
+          <input
+            type="text"
+            placeholder="e.g. Stephen King"
+            id="name"
+            className="info-input"
+          />
+        </div>
+
+        <div className="mb-[20px]">
+          <div className="info-label">
+            <label htmlFor="email">Email Address</label>
+            <p></p>
+          </div>
+
+          <input
+            type="text"
+            placeholder="e.g. stephenking@lorem.com"
+            id="email"
+            className="info-input"
+          />
+        </div>
+
+        <div className="mb-[20px]">
+          <div className="info-label">
+            <label htmlFor="num">Phone Number</label>
+            <p></p>
+          </div>
+
+          <input
+            type="number"
+            placeholder="e.g. +1 234 567 890"
+            id="num"
+            className="info-input"
+          />
+        </div>
+      </div>
     </div>
   );
 };
